df60466c59cbbc3c6f5d25595cae8e477a36d365
pino18 <jpinor@unal.edu.co>
2022-11-07 12:15:46 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -135,7 +135,7 @@ def pregunta_04():
     # considerar 10 valores entre 0.1 y 1.0 para el parámetro alpha de
     # BernoulliNB.
     param_grid = {
-        "BernoulliNB_alpha": np.arrange(0.1, 1.1, 0.1),
+        "BernoulliNB_alpha": np.arange(0.1, 1.1, 0.1),
     }
 
     # Defina una instancia de GridSearchCV con el pipeline y el diccionario de
